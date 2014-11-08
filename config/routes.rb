c3fde710fcd89696820c5e5481a361783a2f0d60
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-11-08 13:28:32 -0500

    users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  #devise_for :users
+  devise_for :users, :controllers => {
+    registrations: 'users/registrations',
+    :sessions => "users/sessions",
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations'
+  }
+  authenticate :user do
+    namespace :users do
+    end
+  end
   match 'ping'         => 'pages#ping',           as: :ping, via: :all
 
   root 'pages#index'
