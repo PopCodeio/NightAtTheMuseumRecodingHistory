e4773eed1ccd7731d1eaae06bf4eedf7a3dabeee
Aldo Delgado <aldo@osstek.com>
2014-11-08 20:58:45 -0500

    WIP Badges

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
   has_merit
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -9,5 +8,4 @@ class User < ActiveRecord::Base
   has_many :tags, through: :user_tags
   has_many :items, through: :user_tags
   has_many :comments
-
 end
