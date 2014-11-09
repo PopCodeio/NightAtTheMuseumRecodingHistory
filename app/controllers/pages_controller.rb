c322954cbdfcac0925215bb28622d5cea931d081
Aldo Delgado <aldo@osstek.com>
2014-11-08 22:29:41 -0500

    fixed merge

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < PublicController
 
-end
+  def index
+    respond_to do |format|
+      format.html
+      #format.json { render json:  }
+    end
+  end
 
+end
--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -1,17 +1,17 @@
 # Use this hook to configure merit parameters
 Merit.setup do |config|
   # Check rules on each request or in background
-  # config.checks_on_each_request = true
+  config.checks_on_each_request = true
 
   # Define ORM. Could be :active_record (default) and :mongoid
-  # config.orm = :active_record
+  config.orm = :active_record
 
   # Add application observers to get notifications when reputation changes.
   # config.add_observer 'MyObserverClassName'
 
   # Define :user_model_name. This model will be used to grand badge if no
   # `:to` option is given. Default is 'User'.
-  # config.user_model_name = 'User'
+  config.user_model_name = 'User'
 
   # Define :current_user_method. Similar to previous option. It will be used
   # to retrieve :user_model_name object if no `:to` option is given. Default
@@ -24,7 +24,7 @@ badge_id = 0
 [{
   id: (badge_id = badge_id+1),
   name: 'just-registered',
-  description: ''
+  description: 'Just registered!'
 }, {
   id: (badge_id = badge_id+1),
   name: 'autobiographer',
