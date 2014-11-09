5fb648f0fc91ebbf8ce876193b5aafabd5a540e6
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-11-09 07:55:41 -0500

    fix name

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
   after_filter :give_badge
 
   def give_badge
-    current_user.add_badge(1)
+    if current_user
+      add_badge = true
+      current_user.badges.each do |b|
+        if b.id == 1
+          add_badge = false
+        end
+      end
+      if add_badge
+        current_user.add_badge(1)
+      end
+    end
   end
 end
