26467ce7f63d12511329e4f6dc53dcb2b2d5fe62
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-11-09 01:45:53 -0500

    Disabled watermark

--- app/uploader/file_uploader.rb
+++ app/uploader/file_uploader.rb
@@ -3,14 +3,18 @@ class FileUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   include CarrierWave::RMagick
   # Process files as they are uploaded:
-  process :watermark
+  #process :watermark
 
-  def watermark
-    manipulate! do |img|
-      logo = Magick::Image.read("#{Rails.root}/app/assets/images/watermark.jpg").first
-      img = img.composite(logo, Magick::NorthWestGravity, 15, 0, Magick::OverCompositeOp)
-    end
-  end
+  #def watermark
+  #  manipulate! do |img|
+  #    logo = Magick::Image.read("#{Rails.root}/app/assets/images/watermark.png").first
+  #    img = img.composite(logo, Magick::SouthEastGravity, 15, 0, Magick::OverCompositeOp)
+  #  end
+  #  manipulate! do |img|
+  #    logo = Magick::Image.read("#{Rails.root}/app/assets/images/HistoryMiami_Logo.png").first
+  #    img = img.composite(logo, Magick::NorthWestGravity, 15, 0, Magick::OverCompositeOp)
+  #  end
+  #end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
--- app/views/items/my_json.json.jbuilder
+++ app/views/items/my_json.json.jbuilder
@@ -1,6 +1,6 @@
 json.timeline do items
   json.headline 'Revolutionary User Interfaces'
-  json.startDate '1600'
+  json.startDate '1700'
   json.text 'test'
   json.type 'default'
   json.asset do
@@ -10,12 +10,12 @@ json.timeline do items
   end
   json.date(items) do |item|
     json.headline item.title
-    json.startDate item.time_line_date
+    json.startDate item.time_line_date.strftime("%F%T")
     json.text item.description
     json.asset do
       json.media item.picture_url
       json.credit item.creator
-      json.caption 'ertwer'
+      json.caption item.rights
     end
   end
 end
