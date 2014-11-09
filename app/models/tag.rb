ee6ee1b770ed0da1e915eaa62624d8b22b2822c0
Aldo Delgado <aldo@osstek.com>
2014-11-09 06:00:49 -0500

    WIP styling

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,7 @@ class Tag < ActiveRecord::Base
   has_many :user_tags
   has_many :users, through: :user_tags
   has_many :items, through: :user_tags
+  before_validation { |tag| tag.name = name.downcase}
 
   def tag_count(options ={})
     self.user_tags.where(item_id: options[:item]).count
