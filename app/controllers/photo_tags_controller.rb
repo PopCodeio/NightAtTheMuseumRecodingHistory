d58c1c1a87e1981aa82db43d6e7adc18c40fda65
Moises Zaragoza <mZaragoza@circletechfl.com>
2014-11-09 07:36:19 -0500

    add badges for adding tags

--- app/controllers/photo_tags_controller.rb
+++ app/controllers/photo_tags_controller.rb
@@ -13,6 +13,37 @@ class PhotoTagsController < PublicController
       tag.save
       ut = UserTag.find_or_initialize_by(user_id: current_user.id, tag_id: tag.id, item_id: item.id )
       ut.save
+      add_badge = true
+      current_user.badges.each do |b|
+        if b.id == 3
+          add_badge = false
+        end
+      end
+      if add_badge
+        current_user.add_badge(3)
+      end
+      if current_user.tags.count >5
+        add_badge = true
+        current_user.badges.each do |b|
+          if b.id == 4
+            add_badge = false
+          end
+        end
+        if add_badge
+          current_user.add_badge(4)
+        end
+      end
+      if current_user.tags.count >25
+        add_badge = true
+        current_user.badges.each do |b|
+          if b.id == 5
+            add_badge = false
+          end
+        end
+        if add_badge
+          current_user.add_badge(5)
+        end
+      end
     end
     if params[:from_page] == 'items'
       redirect_to item_path(item)
